cfd40301e285f748c611207159df1a74accc9afb
Tactenator <trevor.mclaurine@gmail.com>
2023-05-02 19:11:25 -0500

    finished projects page

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const projectOne = document.getElementById('projects-container project0'); 
+    const projectTwo = document.getElementById('projects-container project1'); 
+    const projectThree = document.getElementById('projects-container project2'); 
 
+    const projectsButton = document.getElementById('projects-button')
+    const databaseButton = document.getElementById('database-button')
+    const unitButton = document.getElementById('unit-button')
+
+    const projects = [ projectOne, projectTwo, projectThree ];
+    const buttons = [ projectsButton, databaseButton, unitButton ]
+
+    function handleProjects(e) {
+        if(e.target.classList.contains('selected')){
+            return
+        }
+        else {
+            buttons.forEach(button => {
+                button.classList.remove('selected')
+            })
+            e.target.classList.add('selected')
+            projects.forEach(project => {
+                project.classList.remove('show')
+                project.classList.add('hide')
+                if(project.classList.contains(e.target.innerHTML))
+                {
+                    project.classList.add('show')
+                }
+            })
+        }
+    }
+
+    for(let item of buttons) {
+        item.addEventListener('click', (e) => {
+            handleProjects(e)
+        })
+    }
   
   });
 
